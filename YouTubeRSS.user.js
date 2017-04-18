8af99f583b2f77a1a8224048efb9cce357dc64c2
Wouter Janson <wouterjanson@icloud.com>
2017-04-18 20:10:09 +0200

    Add playlist RSS

--- YouTubeRSS.user.js
+++ YouTubeRSS.user.js
@@ -14,6 +14,7 @@
 
 // Button CSS
 var buttonCSS = "padding: 0 6px; color: #737373; font-size: 11px; text-align: center; margin-left: -2px; border: 1px solid #ccc; background-color: #fafafa; vertical-align: middle; border-radius: 0 2px 2px 0; height: 22px; line-height: 24px; display: inline-block;";
+var playlistButtonCSS = 'font: 12px "YouTube Noto", Roboto, arial, sans-serif; font-weight: 500; background: #f8f8f8; color: #333; border: solid 1px #d3d3d3; display: inline-block; padding: 5.5px 10px; height: 15px; vertical-align: middle; border-radius: 2px; box-shadow: 0 1px 0 rgba(0, 0, 0, 0.05); text-decoration: none;';
 
 // Watches Title elements
 waitForKeyElements("#watch7-headline", videoPageRSS);
@@ -35,4 +36,23 @@ function channelPageRSS () {
         return;
     }
     $('.primary-header-actions').prepend("<a style='" + buttonCSS + "float: right;" + "'href='https://www.youtube.com/feeds/videos.xml?channel_id=" + channelId + "'>RSS Feed</a>");
+}
+
+// Add RSS button on playlist page
+function playlistPageRSS () {
+    var playlistId = getParameterByName('list');
+    if (typeof playlistId === 'undefined') {
+        return;
+    }
+    $('.playlist-actions').append("<a style='" + playlistButtonCSS + "'href='https://www.youtube.com/feeds/videos.xml?playlist_id=" + playlistId + "'>RSS Feed</a>");
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
